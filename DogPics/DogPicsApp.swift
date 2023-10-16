cf1f3bc3c08056ca1eb40e4874e7c745b3b67b72
AntonyBil <beletskiy.ant@gmail.com>
2023-10-16 19:06:25 +0300

    Initial commit

---
+++ DogPics/DogPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicsApp.swift
+//  DogPics
+//
+//  Created by apple on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DogPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
